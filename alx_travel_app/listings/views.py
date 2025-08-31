a8391f9babd95bd578a79b3aa5342bbd9c89a341
Daniel Kweku Addo Arku <danielarku438@gmail.com>
2025-08-31 16:43:17 +0000

    Create views.py

---
+++ alx_travel_app/listings/views.py
@@ -0,0 +1,18 @@
+from rest_framework import viewsets
+from .models import Booking
+from .serializers import BookingSerializer
+from .tasks import send_booking_confirmation
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+    def perform_create(self, serializer):
+        booking = serializer.save()
+        # Trigger async task
+        send_booking_confirmation.delay(
+            booking.customer.email,
+            booking.id,
+            booking.listing.title
+        )
